9d9eaf4e67b3753fec954894a8f3c8401ac6f43a
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-06 13:15:55 +0200

    add component navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Menu from "./components/Menu";
-import CourseGrid from "./components/CourseGrid";
+import Navbar from "./components/Navbar";
 
-// MENU
-const menuRoot = document.getElementById("menu-root");
+const menuRoot = document.getElementById("menu");
+
 if (menuRoot) {
-  ReactDOM.createRoot(menuRoot).render(<Menu />);
-}
-
-// CORSI
-const coursesRoot = document.getElementById("courses-root");
-if (coursesRoot) {
-  ReactDOM.createRoot(coursesRoot).render(<CourseGrid />);
+  ReactDOM.createRoot(menuRoot).render(<Navbar />);
 }
