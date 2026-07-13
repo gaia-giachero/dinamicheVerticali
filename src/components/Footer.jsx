85fdae7901c18dbd02d5b70643ac202987e89c30
gaia-giachero <gaia.gicahero@edu-its.it>
2026-07-13 12:54:41 +0200

    add base for component (wip)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    
+}
+
+// footer contenente logo con info affianco i contatti e i social e sotto le aziende 
---
+++ src/components/PillarPage.jsx
@@ -0,0 +1,8 @@
+export default function PillarPage(){
+
+}
+
+// la pagina importa un componente card (contenente: logo[img], nome corso, botone per vedere di più)
+// il bottone è collegato a una pagina dove vengono renderizzati tutti gli articoli di riferimento a quella categoria (articolo.categoria)
+// le card renderizzate dovranno essere tante quanti corsi esistono + 1 (opzione per visualizzare tutti gli articoli di tutti i corsi in ordine cronologico)
+// la card deve avere annche in questo caso il contorno dello stesso colore di articolo.categoria 
