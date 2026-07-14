010c2a7a3855ed1dbe49f11c40efc1e9006a20fb
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-14 17:07:26 +0200

    feat(footer): create, implement logic and add style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
-export default function Footer(){
-    
-}
+// footer contenente logo con info affianco i contatti e sotto le aziende
+import "../../assets/style/style-footer.css";
 
-// footer contenente logo con info affianco i contatti e i social e sotto le aziende 
+export default function Footer() {
+    return (
+        <footer id="footer">
+            <div className="firstSection">
+                <img src="/img/privacy&policy.svg" alt="dinamiche verticali formazione" loading="lazy" />
+                <div className="contatti">
+                    <p>Contattaci</p>
+                    <p>+39 011 27 32 500</p>
+                    <p>(Lun-Ven dalle 08:30 alle 17:30)</p>
+                    <p>formazione@petzl.it</p>
+                    <p>Via G. Battista Feroggio, 54, 10151 Torino</p>
+                </div>
+            </div>
+            <img src="/img/aziende.svg" alt="petzl, irata, gwo" loading="lazy" />
+        </footer>
+    );
+}
