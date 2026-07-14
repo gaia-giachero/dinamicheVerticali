1274af00b23ad55d27d8a9cf4b1af897080048be
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-14 17:07:57 +0200

    feat(CardCorso): create component (wip)

---
+++ src/components/CardCorso.jsx
@@ -0,0 +1,8 @@
+export default function CardCorso(){
+
+}
+
+// card del singolo corso, cosa deve avere: 
+// contorno con colore corso -> prendere la logica del carosello 
+// tasto per leggere di più -> porta agli articoli di quella categoria
+// una card deve essere  
