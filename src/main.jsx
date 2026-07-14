0259bc00caa299408cb7edb79cbb87e31bd7cf34
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-14 17:58:20 +0200

    refactor: change code structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,26 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar";
 import Carosello from "./components/Carosello";
 import CampoRicerca from "./components/CampoRicerca";
+import Footer from "./components/Footer";
 
 const menuRoot = document.getElementById("menu");
 const caroselloRoot = document.getElementById("carosello");
 const ricercaRoot = document.getElementById("ricerca");
-const pillarPageRoot = document.getElementById("pillar-page");
+// const pillarPageRoot = document.getElementById("pillar-page");
 const footerRoot = document.getElementById("footer");
 
 if (menuRoot) {
   ReactDOM.createRoot(menuRoot).render(<Navbar />);
+}
+
+if (caroselloRoot) {
   ReactDOM.createRoot(caroselloRoot).render(<Carosello />);
+}
+
+if (ricercaRoot) {
   ReactDOM.createRoot(ricercaRoot).render(<CampoRicerca />);
-  ReactDOM.createRoot(pillarPageRoot).render(<PillarPage />);
+}
+
+if (footerRoot) {
   ReactDOM.createRoot(footerRoot).render(<Footer />);
 }
