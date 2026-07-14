444fa685399c61e889f462733aadc3e984c35308
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-14 17:46:13 +0200

    style(CampoRicerca): add style to component

--- src/components/CampoRicerca.jsx
+++ src/components/CampoRicerca.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
+import "../../assets/style/style-campo-ricerca.css";
 
-export default function CampoRicerca({id, children, ref, ...props}) {
-  const[input, setInput] = useState("");
+export default function CampoRicerca({ id, children, ref, ...props }) {
+  const [input, setInput] = useState("");
 
   function handleChangeInput(i) {
     setInput(i.target.value);
   }
 
   return (
-    <div>
-      <input id={id} type="text" value={input} onChange={handleChangeInput} ref={ref} placeholder="Cerca l'articolo che fa per te" className="outline-none w-full" />
+    <div className="campo-ricerca">
+      <input 
+        id={id} 
+        type="text" 
+        value={input} 
+        onChange={handleChangeInput} 
+        ref={ref} 
+        placeholder="Cerca l'articolo che fa per te" 
+        className="input" 
+      />
       {children}
     </div>
   );
 }
 
-// il campo di ricerca deve effettuare una ricerca 
-// e deve dare un risultato se la parola inserita è presente nei RIFERIMENTI dell'articolo 
+// il campo di ricerca deve effettuare una ricerca
+// e deve dare un risultato se la parola inserita è presente nei RIFERIMENTI dell'articolo
 // altrimenti da un messaggio di errore tipo: "Nessuna corrispondenza con il campo inserito!"
