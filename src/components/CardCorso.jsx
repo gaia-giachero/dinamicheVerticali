c86375169dbb306cc41de98ee0f37b0940ed3e5c
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-16 12:47:27 +0200

    feat(PillarPage): create and implement pillarPage

--- src/components/CardCorso.jsx
+++
@@ -1,8 +0,0 @@
-export default function CardCorso(){
-
-}
-
-// card del singolo corso, cosa deve avere: 
-// contorno con colore corso -> prendere la logica del carosello 
-// tasto per leggere di più -> porta agli articoli di quella categoria
-// una card deve essere  
--- src/components/PillarPage.jsx
+++ src/components/PillarPage.jsx
@@ -1,8 +1,61 @@
-export default function PillarPage(){
+import React, { useEffect, useState } from "react";
+import "../../assets/style/style-pillar-page.css";
 
+const colori = {
+    "Fune D.Lgs. 81/08": "#3DB876",
+    "GWO": "#EF546C",
+    IRATA: "#33658A",
+    "Lavori in Quota": "#FFCD09",
+    "PTI": "#212529",
+    "Soccorso": "#DC3545",
+    "Corsi accreditati": "#17BEBB",
+    "Default": "#ccc",
+};
+
+const page = [
+    { id: 1, titolo: "Fune D.Lgs. 81/08", categoria: "Fune D.Lgs. 81/08", image: "fune.webp" },
+    { id: 2, titolo: "GWO", categoria: "GWO", image: "gwo.webp" },
+    { id: 3, titolo: "IRATA", categoria: "IRATA", image: "irata.webp" },
+    { id: 4, titolo: "Lavori in Quota", categoria: "Lavori in Quota", image: "lavori-in-quota.webp" },
+    { id: 5, titolo: "PTI", categoria: "PTI", image: "pti.webp" },
+    { id: 6, titolo: "Soccorso", categoria: "Soccorso", image: "itra.webp" },
+    { id: 7, titolo: "Corsi accreditati", categoria: "Corsi accreditati", image: "corsi-accreditati.webp" }
+]
+
+export default function PillarPage() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    return (
+        <div className="grid-container">
+            {
+                page.map((pagina) => {
+                    // colore del bordo in base alla categoria
+                    const bordo = colori[pagina.categoria] || colori.Default;
+
+                    return (
+                        <div
+                            className="card"
+                            key={pagina.id}
+                            style={{ borderColor: bordo }}
+                        >
+                            <img src={`/img/corsi/${pagina.image}`} alt={pagina.titolo} />
+                            <div className="card-dati">
+                                <h3>{pagina.titolo}</h3>
+                                <a
+
+                                    href={`/corso/${pagina.id}`}
+                                    className="btn-more"
+                                    style={{ color: bordo }}
+                                >
+                                    Leggi di più
+                                </a>
+                            </div>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 
-// la pagina importa un componente card (contenente: logo[img], nome corso, botone per vedere di più)
-// il bottone è collegato a una pagina dove vengono renderizzati tutti gli articoli di riferimento a quella categoria (articolo.categoria)
-// le card renderizzate dovranno essere tante quanti corsi esistono + 1 (opzione per visualizzare tutti gli articoli di tutti i corsi in ordine cronologico)
-// la card deve avere annche in questo caso il contorno dello stesso colore di articolo.categoria 
+// card del singolo corso, cosa deve avere:
+// contorno con colore corso -> prendere la logica del carosello
+// tasto per leggere di più -> porta agli articoli di quella categoria
