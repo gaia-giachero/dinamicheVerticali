53a100b58ed69c3cf580587c4637587e7b986245
Gaia Giachero <gaia.giachero@edu-its.it>
2026-07-16 12:49:17 +0200

    feat(main): add controlo for render pillar page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar";
 import Carosello from "./components/Carosello";
 import CampoRicerca from "./components/CampoRicerca";
+import PillarPage from "./components/PillarPage";
 import Footer from "./components/Footer";
 
 const menuRoot = document.getElementById("menu");
 const caroselloRoot = document.getElementById("carosello");
 const ricercaRoot = document.getElementById("ricerca");
-// const pillarPageRoot = document.getElementById("pillar-page");
+const pillarPageRoot = document.getElementById("pillar-page");
 const footerRoot = document.getElementById("footer");
 
 if (menuRoot) {
@@ -21,6 +22,10 @@ if (caroselloRoot) {
 
 if (ricercaRoot) {
   ReactDOM.createRoot(ricercaRoot).render(<CampoRicerca />);
+}
+
+if (pillarPageRoot) {
+  ReactDOM.createRoot(pillarPageRoot).render(<PillarPage />);
 }
 
 if (footerRoot) {
